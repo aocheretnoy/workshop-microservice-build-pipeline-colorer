da076e70813d59871a44be5db2d422d8e92452fa
arkady.ocheretnoy <aocheretnoy@eastbanctech.com>
2018-06-15 14:24:15 +0400

    Code updated

--- pkg/colorer/colorer-handler.go
+++ pkg/colorer/colorer-handler.go
@@ -14,7 +14,7 @@ type colorerServer struct {
 func (s *colorerServer) GetColor(ctx context.Context, msg *GetColorRequest) (*GetColorResponse, error) {
 	log.Printf("Server colorer called with message (%v)", msg)
 	time.Sleep(time.Duration(500) * time.Millisecond)
-	return &GetColorResponse{Cold: 10, Hot: 90}, nil
+	return &GetColorResponse{Cold: 0, Hot: 100}, nil
 }
 
 func NewServer() ColorerServer {
